1f618489c07315177a56724ee3937058ce8eed5b
Artur Serra <artur_seilah@hotmail.com>
2022-11-17 19:21:59 -0300

    Created the constants.js and connected the contract ABI to the frontend

---
+++ client/lib/constants.js
@@ -0,0 +1,4 @@
+import contractArtifact from "./ProfileImageNft.json";
+
+export const contractAddress = "0x31eE092E97775A43738d289D33f789c2eC2677C5";
+export const contractABI = contractArtifact.abi
